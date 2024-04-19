7a45649c8d162b0d4c64d5c26d104c5cfe834a89
Niranj Chandrasekaran <srinivasniranj@gmail.com>
2024-04-19 17:14:29 -0400

    UMAP plots

--- 04.create-visualization/utils.py
+++ 04.create-visualization/utils.py
@@ -38,11 +38,21 @@ def infer_platemap(df):
 
     return platemap_df
 
+def get_metacols(df):
+    """return a list of metadata columns"""
+    return [c for c in df.columns if c.startswith("Metadata_")]
 
-def identify_control_plate(df):
-    test = (
-        df.groupby("Plate")
-        .agg({"Metadata_JCP2022": "nunique"})
-    )
 
-    print(test)
+def get_featurecols(df):
+    """returna  list of featuredata columns"""
+    return [c for c in df.columns if not c.startswith("Metadata")]
+
+
+def get_metadata(df):
+    """return dataframe of just metadata columns"""
+    return df[get_metacols(df)]
+
+
+def get_featuredata(df):
+    """return dataframe of just featuredata columns"""
+    return df[get_featurecols(df)]
