3be9da6e085010170d7f7157b2b304dfdf29eb32
Niranj Chandrasekaran <srinivasniranj@gmail.com>
2024-06-19 15:17:50 -0400

    plot features

---
+++ 10.feature-wise-analyses/utils.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import numpy as np
+from sklearn.utils.validation import check_is_fitted
+from sklearn.utils import check_array, as_float_array
+from sklearn.base import TransformerMixin, BaseEstimator
+import scipy
+from sklearn.metrics import average_precision_score
+from sklearn.metrics.pairwise import cosine_similarity
+from tqdm import tqdm
+import random
+import itertools
+
+
+def get_metacols(df):
+    """return a list of metadata columns"""
+    return [c for c in df.columns if c.startswith("Metadata_")]
+
+
+def get_featurecols(df):
+    """returna  list of featuredata columns"""
+    return [c for c in df.columns if not c.startswith("Metadata")]
+
+
+def get_metadata(df):
+    """return dataframe of just metadata columns"""
+    return df[get_metacols(df)]
+
+
+def get_featuredata(df):
+    """return dataframe of just featuredata columns"""
+    return df[get_featurecols(df)]
+
+
+def remove_negcon_empty_wells(df):
+    """return dataframe of non-negative control wells"""
+    df = (
+        df.query('Metadata_pert_type!="negcon"')
+        .dropna(subset=["Metadata_broad_sample"])
+        .dropna(subset=["Metadata_Symbol"])
+        .reset_index(drop=True)
+    )
+    return df
+
+
+def remove_low_infection_efficiency_wells(df):
+    """return dataframe of non-low infection efficiency wells"""
+
+    reagents_to_remove_df = pd.read_csv(
+        "../00.download-and-process-annotations/output/orf-reagents-low-infection-efficiency-and-outliers.csv.gz",
+        usecols=["Metadata_plate_map_name", "Metadata_Well", "Metadata_broad_sample"],
+    )
+
+    df = (
+        df.merge(
+            reagents_to_remove_df,
+            on=["Metadata_plate_map_name", "Metadata_Well", "Metadata_broad_sample"],
+            how="left",
+            indicator=True,
+        )
+        .query('_merge=="left_only"')
+        .drop(columns=["_merge"])
+    )
+
+    return df
+
+
+def remove_empty_wells(df):
+    """return dataframe of non-empty wells"""
+    df = df.query("Metadata_pert_type!='empty'").reset_index(drop=True)
+    df = df.dropna(subset=["Metadata_broad_sample"]).reset_index(drop=True)
+    df = df.dropna(subset=["Metadata_Symbol"]).reset_index(drop=True)
+    return df
+
+
+def concat_profiles(df1, df2):
+    """Concatenate dataframes"""
+    if df1.shape[0] == 0:
+        df1 = df2.copy()
+    else:
+        frames = [df1, df2]
+        df1 = pd.concat(frames, ignore_index=True, join="inner")
+
+    return df1
+
+
+def consensus(profiles_df, group_by_feature):
+    """
+    Computes the median consensus profiles.
+    Parameters:
+    -----------
+    profiles_df: pandas.DataFrame
+        dataframe of profiles
+    group_by_feature: str
+        Name of the column
+    Returns:
+    -------
+    pandas.DataFrame 
+    """
+
+    metadata_df = get_metadata(profiles_df).drop_duplicates(subset=[group_by_feature])
+
+    feature_cols = [group_by_feature] + get_featurecols(profiles_df)
+    profiles_df = (
+        profiles_df[feature_cols].groupby([group_by_feature]).median().reset_index()
+    )
+
+    profiles_df = metadata_df.merge(profiles_df, on=group_by_feature)
+
+    return profiles_df
+
+
+def remove_nan_features(df):
+    """
+    Remove NaN features.
+    Parameters
+    ----------
+    df: pandas.DataFrame
+        DataFrame of profiles.
+    Returns
+    -------
+    df: pandas.DataFrame
+        DataFrame of profiles after dropping NaN features.
+    """
+
+    r, c = np.where(df.isna())
+    features_to_remove = [
+        _ for _ in list(df.columns[list(set(c))]) if not _.startswith("Metadata_")
+    ]
+    print(f'Removed nan features: {features_to_remove}')
+    df.drop(features_to_remove, axis=1, inplace=True)
+    return df
